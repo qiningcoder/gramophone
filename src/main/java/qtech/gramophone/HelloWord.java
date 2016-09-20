7a95df06e2cf1eef021087638bfd411e9bf70f69
shiqining <shiqining@lianjia.com>
2016-09-20 10:07:57 +0800

    Rejected commit: Default

---
+++ src/main/java/qtech/gramophone/HelloWord.java
@@ -0,0 +1,10 @@
+package qtech.gramophone;
+
+/**
+ * Created by shiqining on 9/20/16.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
