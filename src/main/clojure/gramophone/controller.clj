7d7080900015e44baa1d847a4e62734d7d5f7642
shiqining <shiqining@lianjia.com>
2016-09-21 11:24:49 +0800

    use RequestParam on HelloController

--- src/main/clojure/gramophone/controller.clj
+++ src/main/clojure/gramophone/controller.clj
@@ -1,14 +1,14 @@
 (ns gramophone.controller
   (:import (org.springframework.stereotype Controller)
-           (org.springframework.web.bind.annotation RequestMapping RequestMethod ResponseBody)
+           (org.springframework.web.bind.annotation RequestMapping RequestMethod ResponseBody RequestParam)
            (javax.servlet.http HttpServletRequest)))
 
 (gen-class
   :name ^{Controller ""} gramophone.controller.HelloController
   :methods [[^{RequestMapping {:value  ["/hello"]
                                :method [RequestMethod/GET]}
-               ResponseBody {}} hello [javax.servlet.http.HttpServletRequest] String]])
+               ResponseBody {}} hello [^{RequestParam {:value "name"}} String] String]])
 
 (defn -hello
-  [this ^HttpServletRequest request]
-  (str (.getParameter request "name") "hello" "world"))
+  [this ^String name]
+  (str name #_(.getParameter request "name") "hello" "world"))
